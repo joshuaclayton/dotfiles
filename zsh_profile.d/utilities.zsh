e5a857a1ebdb1993b106b0e5efab67a464bd72c0
Joshua Clayton <jclayton@thoughtbot.com>
2010-12-01 22:17:17 -0500

    Make cs work for Rails 3 apps too

--- zsh_profile.d/utilities.zsh
+++ zsh_profile.d/utilities.zsh
@@ -1,3 +1,14 @@
 _() {
   test $? -eq 0;
 }
+
+cs() {
+  if [[ -f script/rails ]]; then
+    (script/rails console)
+  elif [[ -f script/console ]]; then
+    (script/console)
+  else;
+    print Not a Rails application. Aborting...
+    return 1
+  fi
+}
