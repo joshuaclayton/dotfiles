6e10087635295bc3527744dca7e4961f2ef5b926
Joshua Clayton <jclayton@thoughtbot.com>
2010-12-16 16:52:29 -0500

    Display vim mode in zsh

--- zsh_profile.d/zprompt.zsh
+++ zsh_profile.d/zprompt.zsh
@@ -70,5 +70,23 @@ _colored_path()           { echo "$(_grey "%~")" }
 _colored_git_branch()     { echo "$(_git_prompt_color "$(_git_prompt_info)")" }
 _colored_git_difference() { echo "$(_yellow "$(_git_difference_from_track)")" }
 
+display_current_vim_mode() {
+  if [[ $VIMODE == 'vicmd' ]]; then
+    RPROMPT="-NORMAL-"
+  else
+    RPROMPT="-INSERT-"
+  fi
+}
+
+function zle-line-init zle-keymap-select {
+  VIMODE=$KEYMAP
+  display_current_vim_mode
+  zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
+display_current_vim_mode
+
 # prompt
 export PS1='$(_bracket_wrap "$(_basic)$(_separate $(_colored_git_branch))$(_separate $(_colored_git_difference))")'
