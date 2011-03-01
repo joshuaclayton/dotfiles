4b01d14b5f73851c762a6dad79c6bb374a476f02
Joshua Clayton <jclayton@thoughtbot.com>
2011-03-01 11:14:03 -0500

    Benchmark average

--- irbrc.d/helpers.rb
+++ irbrc.d/helpers.rb
@@ -8,6 +8,16 @@ def benchmark
   "#{(Time.now - t1) * 1000}ms"
 end
 
+def benchmark_average(times = 20)
+  average = []
+  times.times do
+    average << benchmark do
+      yield
+    end.to_f
+  end
+  "#{average.inject {|sum, item| sum += item }/times.to_f}ms"
+end
+
 class MemoryUsage
   def self.kilobytes
     `ps -o rss= -p #{$$}`.to_i
