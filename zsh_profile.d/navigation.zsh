fad36c9e6684666560fbafa2f8cbf6533300a27e
Joshua Clayton <jclayton@thoughtbot.com>
2012-03-02 00:05:57 -0500

    Fix RVM + auto-cd to last directory for binstubs

--- zsh_profile.d/navigation.zsh
+++ zsh_profile.d/navigation.zsh
@@ -7,11 +7,3 @@ function chpwd {
   echo $(pwd) >! $CURRENT_PROJECT_PATH
   ls
 }
-
-current() {
-  if [[ -f $CURRENT_PROJECT_PATH ]]; then
-    cd "$(cat $CURRENT_PROJECT_PATH)"
-  fi
-}
-
-current
