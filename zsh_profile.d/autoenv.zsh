64f4f75f9c33e34599cd106f72f114db7966bcd2
Joshua Clayton <jclayton@thoughtbot.com>
2013-08-08 21:38:21 -0400

    Fix modifying the .env_auth file

--- zsh_profile.d/autoenv.zsh
+++ zsh_profile.d/autoenv.zsh
@@ -21,7 +21,7 @@ _dotenv_authorize() {
 
 _dotenv_deauthorize() {
   env_file=$1
-  \grep -Gv $env_file $ENV_AUTHORIZATION_FILE > $ENV_AUTHORIZATION_FILE
+  echo $(grep -Gv $env_file $ENV_AUTHORIZATION_FILE) > $ENV_AUTHORIZATION_FILE
 }
 
 _dotenv_print_unauthorized_message() {
