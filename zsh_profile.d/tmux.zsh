3f4a9d47bc7413d68f35c35398cee35865c292ae
Joshua Clayton <jclayton@thoughtbot.com>
2013-08-23 08:53:30 -0400

    Automatically attach to a tmux session if not already in tmux

---
+++ zsh_profile.d/tmux.zsh
@@ -0,0 +1,20 @@
+_is_tmux_not_running() {
+  [[ -z $(echo $TMUX) ]]
+}
+
+_is_tmux_session_list_empty() {
+  [[ -z $(tmux ls) ]]
+}
+
+_ensure_tmux_is_running() {
+  if _is_tmux_session_list_empty; then
+    tmux new -d
+  fi
+}
+
+attach_to_tmux() {
+  if _is_tmux_not_running; then
+    _ensure_tmux_is_running
+    tmux att
+  fi
+}
--- zshrc
+++ zshrc
@@ -5,3 +5,4 @@ for zsh_source in $HOME/.zsh_profile.d/*.zsh; do
 done
 
 cd_to_most_recently_opened_directory
+attach_to_tmux
