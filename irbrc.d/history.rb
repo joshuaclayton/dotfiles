ba3ed0e7c2ca2f348f196acab9027becc47254f0
Joshua Clayton <jclayton@thoughtbot.com>
2014-04-02 14:24:00 -0400

    Let's get more historical

--- irbrc.d/history.rb
+++ irbrc.d/history.rb
@@ -20,5 +20,5 @@ module Readline
 end
 
 require "irb/ext/save-history"
-IRB.conf[:SAVE_HISTORY] = 1000
+IRB.conf[:SAVE_HISTORY] = 40000
 IRB.conf[:HISTORY_FILE] = "#{ENV["HOME"]}/.irb-save-history"
