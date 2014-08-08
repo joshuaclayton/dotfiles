9837bb0f078047f71aeb713f0bf1f8f0b4a9ba0b
Joshua Clayton <jclayton@thoughtbot.com>
2014-08-07 20:36:36 -0400

    Clean up parse_tags and ag tab completion

--- zsh_profile.d/completion.zsh
+++ zsh_profile.d/completion.zsh
@@ -108,9 +108,7 @@ compdef _cheat cheat
 
 _ack() {
   if (( CURRENT == 2 )); then
-    if [[ -a tmp/tags || -a .git/tags ]]; then
-      compadd $(parse_tags)
-    fi
+    compadd $(parse_tags)
   else;
     _files
   fi
