d7709f375f11414b7832093b3a06e050b5311b17
Joshua Clayton <jclayton@thoughtbot.com>
2015-03-23 16:47:58 -0400

    Factor in feedback from ShellCheck

--- zsh_profile.d/navigation.zsh
+++ zsh_profile.d/navigation.zsh
@@ -4,12 +4,12 @@ setopt auto_cd
 cdpath=($HOME/dev $HOME/dev/thoughtbot $HOME/dev/gems $HOME/dev/personal)
 
 function chpwd {
-  echo $(pwd) >! $CURRENT_PROJECT_PATH
+  echo "$(pwd)" >! "$CURRENT_PROJECT_PATH"
   ls
 }
 
 cd_to_most_recently_opened_directory() {
-  if [[ -f $CURRENT_PROJECT_PATH && -d "$(cat $CURRENT_PROJECT_PATH)" ]]; then
-    cd "$(cat $CURRENT_PROJECT_PATH)"
+  if [[ -f $CURRENT_PROJECT_PATH && -d "$(cat "$CURRENT_PROJECT_PATH")" ]]; then
+    cd "$(cat "$CURRENT_PROJECT_PATH")"
   fi
 }
