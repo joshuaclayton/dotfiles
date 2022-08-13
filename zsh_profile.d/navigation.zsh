45d3000e616f495c4db1222c9ba2745606740307
Josh Clayton <joshua.clayton@gmail.com>
2022-08-13 12:11:38 -0400

    Allow per-machine configuration of additional cdpaths
    What?
    =====
    
    Rather than having to check in custom cdpaths into this repository, this
    allows for local configuration to add cdpaths.
    
    It's worth noting that this does not expand e.g. `$HOME`; the full path
    is required to work as expected.

--- zsh_profile.d/navigation.zsh
+++ zsh_profile.d/navigation.zsh
@@ -3,6 +3,12 @@ export CURRENT_PROJECT_PATH=$HOME/.current-project
 setopt auto_cd
 cdpath=($HOME/dev $HOME/dev/unused $HOME/dev/gems $HOME/dev/personal)
 
+if [[ -f ~/.cdpath.local ]]; then
+  while read -r line
+  do cdpath=($line $cdpath)
+  done < ~/.cdpath.local
+fi
+
 function chpwd {
   echo "$(pwd)" >! "$CURRENT_PROJECT_PATH"
   local v=$(projections)
