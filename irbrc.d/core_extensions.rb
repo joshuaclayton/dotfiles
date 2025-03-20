3b634511f89fa38423669f61012fef70f8f1409f
Josh Clayton <joshua.clayton@gmail.com>
2025-03-19 22:27:39 -0400

    Add irb timer

--- irbrc.d/core_extensions.rb
+++ irbrc.d/core_extensions.rb
@@ -19,3 +19,48 @@ class File
     File.open(path, "w") { |file| file << string }
   end
 end
+
+class Timer
+  def initialize(report_every: 10)
+    @report_every = report_every
+    @past_minute = []
+    @per_minute_average = []
+  end
+
+  def time(&block)
+    loop do
+      count = block.call
+      @past_minute.unshift(count)
+      per_minute = handled_per_minute
+      @per_minute_average.unshift(per_minute)
+      puts "Count: #{count} (#{per_minute}/min, #{per_minute_average}/min avg, ~#{minutes_remaining(count)}min remaining)"
+      sleep @report_every
+    end
+  end
+
+  def minutes_remaining(count)
+    if per_minute_average > 0
+      count / per_minute_average
+    else
+      0
+    end
+  end
+
+  def per_minute_average
+    array = @per_minute_average.take(past_minute_count)
+    if array.length > 0
+      array.sum / array.length
+    else
+      0
+    end
+  end
+
+  def past_minute_count
+    60 / @report_every
+  end
+
+  def handled_per_minute
+    most_recent = @past_minute.take(past_minute_count)
+    most_recent.max - most_recent.min
+  end
+end
